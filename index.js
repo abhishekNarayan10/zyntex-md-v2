460bc4bb5e7d177e44814db16855a1a2a12519c1
Zynt3x! <147066801+Zynt3x@users.noreply.github.com>
2023-10-25 18:19:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { Google, Musixmatch } = require("@flytri/lyrics-finder")
 const c = require('ansi-colors')
 var figlet = require("figlet");
 const readline = require('readline');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpegPath = require('@ffmpeg-installer/darwin-arm64').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath)
 const yts = require( 'yt-search' )
