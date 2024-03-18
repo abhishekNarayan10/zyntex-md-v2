cfe898320a3ec590e31e898dce2bbd842d321513
Zynt3x! <147066801+Zynt3x@users.noreply.github.com>
2024-03-18 20:52:49 +0530

    Update alive_img.js

--- assets/Alive Images/alive_img.js
+++ assets/Alive Images/alive_img.js
@@ -1,4 +1,4 @@
-let alive_imgs = ['img1.mp3' , 'img2.mp3' , 'img3.mp3' , 'img4.mp3']
+let alive_imgs = ['img1.jpg' , 'img2.png' , 'img3.jpg' , 'img4.jpg']
 let alive_img = Math.floor(Math.random()* alive_imgs.length)
 const randomAliveImages = './assets/Alive Images/' + alive_imgs[alive_img]
 
