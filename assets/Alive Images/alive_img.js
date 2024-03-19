9452e8af2ee77e343a26c1ce7cd1b5690867519e
Zynt3x! <147066801+Zynt3x@users.noreply.github.com>
2024-03-19 08:55:25 +0530

    Update alive_img.js

--- assets/Alive Images/alive_img.js
+++ assets/Alive Images/alive_img.js
@@ -1,6 +1,5 @@
 let alive_imgs = ['img1.jpg' , 'img2.png' , 'img3.jpg' , 'img4.jpg']
-let alive_img = Math.floor(Math.random()* alive_imgs.length)
-const randomAliveImages = './assets/Alive Images/' + alive_imgs[alive_img]
-
+let alive_img = alive_imgs[(Math.floor(Math.random() * alive_imgs.length))]
+const randomAliveImages = './assets/Alive Images/' + alive_img
 
 module.exports = randomAliveImages
