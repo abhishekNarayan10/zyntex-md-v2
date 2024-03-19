df64008de2e50848e393c29b520aca2228db9107
Zynt3x! <147066801+Zynt3x@users.noreply.github.com>
2024-03-19 08:54:29 +0530

    Update bgm.js

--- assets/Bgm/bgm.js
+++ assets/Bgm/bgm.js
@@ -1,6 +1,6 @@
 let bgms = ['bgm1.mp3' , 'bgm2.mp3' , 'bgm3.mp3' , 'bgm4.mp3' , 'bgm5.mp3']
-let bgm = Math.floor(Math.random()* bgms.length)
-const randomBgm = './assets/Bgm/' + bgms[bgm]
+let bgm = bgms[(Math.floor(Math.random() * bgms.length))]
+const randomBgm = './assets/Bgm/' + bgm
 
 
 module.exports = randomBgm
