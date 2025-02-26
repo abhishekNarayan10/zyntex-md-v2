29f958eb00b4538565e50b3a0c783f9cefecc6f5
!abhishekNarayan <147066801+abhishekNarayan10@users.noreply.github.com>
2025-02-26 09:17:38 +0000

    :)

--- index.js
+++ index.js
@@ -448,28 +448,28 @@ async function zyntex() {
 
                   
 
-                  async function send() {
-                    await zyn.sendMessage(
-                      id,
-                      {
-                        audio: { url: fileName },
-                        mimetype: "audio/mp4",
-                        contextInfo: {
-                          externalAdReply: {
-                            title: videoTitle,
-                            body: botName,
-                            thumbnailUrl: r,
-                            mediaType: 1,
-                            showAdAttribution: true,
-                            renderLargerThumbnail: false,
-                            sourceUrl: res.videoDetails.video_url,
-                          },
-                        },
-                      },
-                      { quoted: q }
-                    );
-                  }
-                  send();
+                  // async function send() {
+                  //   await zyn.sendMessage(
+                  //     id,
+                  //     {
+                  //       audio: { url: fileName },
+                  //       mimetype: "audio/mp4",
+                  //       contextInfo: {
+                  //         externalAdReply: {
+                  //           title: videoTitle,
+                  //           body: botName,
+                  //           thumbnailUrl: r,
+                  //           mediaType: 1,
+                  //           showAdAttribution: true,
+                  //           renderLargerThumbnail: false,
+                  //           sourceUrl: res.videoDetails.video_url,
+                  //         },
+                  //       },
+                  //     },
+                  //     { quoted: q }
+                  //   );
+                  // }
+                  // send();
                 });
               });
             });
